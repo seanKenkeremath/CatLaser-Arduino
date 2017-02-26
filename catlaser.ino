b739caecce679fa94fb5c6bedb5f4f8a491e85df
seanKenkeremath <sean.kenkeremath@willowtreeapps.com>
2017-02-26 18:09:59 -0500

    Using switch to turn on instead of IR receiver

--- catlaser.ino
+++ catlaser.ino
@@ -1,19 +1,12 @@
 
 #include <Servo.h>
-#include "IRremote.h"
 
-enum ButtonPress
-{
-  POWER,
-  PLUS,
-  MINUS
-};
 
 //Config
 const int SERVO_PIN_VERT = 9;
 const int SERVO_PIN_HORZ = 6;
 const int LASER_PIN = 4;
-const int IR_PIN = 11;
+const int SWITCH_PIN = 2;
 
 const int ANGLE_MAX_VERT = 80;
 const int ANGLE_MIN_VERT = 10;
@@ -29,9 +22,6 @@ const int LOOP_DELAY = 10;
 const int JITTER_COUNT_MAX = 5; //How many loops before the jitter direciton is reset
 const float JITTER_SENSITIVITY_MAX = .3 * LOOP_DELAY; //How intense the jitter is (how far it can wander in a single loop)
 const int JITTER_PAUSE_MAX = 10; //How long of a pause is taken between jitters. Random number between 0 and this is taken
-
-IRrecv irrecv(IR_PIN);
-decode_results ir_results;
 
 Servo servoVert;
 Servo servoHorz;
@@ -55,7 +45,7 @@ void setup ()
   servoHorz.attach(SERVO_PIN_HORZ);
   pinMode(LASER_PIN, OUTPUT);
   digitalWrite(LASER_PIN, LOW);
-  irrecv.enableIRIn();
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
   calculateJitterSensitivity();
   recenter();
 }
@@ -73,40 +63,57 @@ void recenter ()
 
 void loop()
 {
-  if (irrecv.decode(&ir_results))
+//  if (irrecv.decode(&ir_results))
+//  {
+//    ButtonPress button = translateIR();
+//    switch (button)
+//    {
+//      case POWER: laserOn = !laserOn;
+//        if (laserOn)
+//        {
+//          digitalWrite(LASER_PIN, HIGH);
+//        }
+//        else
+//        {
+//          recenter();
+//          digitalWrite(LASER_PIN, LOW);
+//        }
+//        break;
+//      case PLUS:
+//        jitterLevel++;
+//        if (jitterLevel > JITTER_LEVEL_MAX)
+//        {
+//          jitterLevel = JITTER_LEVEL_MAX;
+//        }
+//        calculateJitterSensitivity();
+//        break;
+//      case MINUS:
+//        jitterLevel--;
+//        if (jitterLevel < JITTER_LEVEL_MIN)
+//        {
+//          jitterLevel = JITTER_LEVEL_MIN;
+//        }
+//        calculateJitterSensitivity();
+//        break;
+//    }
+//    irrecv.resume(); // receive the next value
+//  }
+
+  boolean switchOn = digitalRead(SWITCH_PIN) == LOW;
+
+  if (switchOn != laserOn)
   {
-    ButtonPress button = translateIR();
-    switch (button)
+    //switch has changed
+    laserOn = switchOn;
+    recenter();
+    if (laserOn)
     {
-      case POWER: laserOn = !laserOn;
-        if (laserOn)
-        {
-          digitalWrite(LASER_PIN, HIGH);
-        }
-        else
-        {
-          recenter();
-          digitalWrite(LASER_PIN, LOW);
-        }
-        break;
-      case PLUS:
-        jitterLevel++;
-        if (jitterLevel > JITTER_LEVEL_MAX)
-        {
-          jitterLevel = JITTER_LEVEL_MAX;
-        }
-        calculateJitterSensitivity();
-        break;
-      case MINUS:
-        jitterLevel--;
-        if (jitterLevel < JITTER_LEVEL_MIN)
-        {
-          jitterLevel = JITTER_LEVEL_MIN;
-        }
-        calculateJitterSensitivity();
-        break;
+      digitalWrite(LASER_PIN, HIGH);
     }
-    irrecv.resume(); // receive the next value
+    else
+    {
+      digitalWrite(LASER_PIN, LOW);
+    }
   }
 
   if (!laserOn || millis() - lastLoopMillis < LOOP_DELAY)
@@ -128,16 +135,6 @@ void loop()
     servoAngleVert += currJitterAngleVert;
     servoAngleHorz += currJitterAngleHorz;
   }
-
-//  Serial.println("Jitter Level");
-//  Serial.println(jitterLevel, DEC);
-//  Serial.println("Jitter Sensitivity");
-//  Serial.println(jitterSensitivity, DEC);
-//  Serial.println("Servo Angle Vert");
-//  Serial.println(servoAngleVert, DEC);
-//  Serial.println("Servo Angle Horz");
-//  Serial.println(servoAngleHorz, DEC);
-
 
   jitterCount--;
 
@@ -162,102 +159,5 @@ void loop()
   servoHorz.write(servoAngleHorz);
   lastLoopMillis = millis();
   delay(LOOP_DELAY);
-}
-
-ButtonPress translateIR()
-{
-
-  switch (ir_results.value)
-  {
-
-    case 0xE318261B:
-    case 0xFFA25D:
-      Serial.println(" CH-            ");
-      return POWER;
-
-    case 0xFF629D:
-      Serial.println(" CH             ");
-      break;
-
-    case 0xFFE21D:
-      Serial.println(" CH+            ");
-      break;
-
-    case 0xFF22DD:
-      Serial.println(" PREV           ");
-      break;
-
-    case 0xFF02FD:
-      Serial.println(" NEXT           ");
-      break;
-
-    case 0xFFC23D:
-      Serial.println(" PLAY/PAUSE     ");
-      break;
-    case 0xF076C13B:
-    case 0xFFE01F:
-      Serial.println(" VOL-           ");
-      return MINUS;
-    case 0xA3C8EDDB:
-    case 0xFFA857:
-      Serial.println(" VOL+           ");
-      return PLUS;
-
-    case 0xFF906F:
-      Serial.println(" EQ             ");
-      break;
-
-    case 0xFF6897:
-      Serial.println(" 0              ");
-      break;
-
-    case 0xFF9867:
-      Serial.println(" 100+           ");
-      break;
-
-    case 0xFFB04F:
-      Serial.println(" 200+           ");
-      break;
-
-    case 0xFF30CF:
-      Serial.println(" 1              ");
-      break;
-
-    case 0xFF18E7:
-      Serial.println(" 2              ");
-      break;
-
-    case 0xFF7A85:
-      Serial.println(" 3              ");
-      break;
-
-    case 0xFF10EF:
-      Serial.println(" 4              ");
-      break;
-
-    case 0xFF38C7:
-      Serial.println(" 5              ");
-      break;
-
-    case 0xFF5AA5:
-      Serial.println(" 6              ");
-      break;
-
-    case 0xFF42BD:
-      Serial.println(" 7              ");
-      break;
-
-    case 0xFF4AB5:
-      Serial.println(" 8              ");
-      break;
-
-    case 0xFF52AD:
-      Serial.println(" 9              ");
-      break;
-
-    default:
-      Serial.println(" other button   ");
-
-  }
 }
 
